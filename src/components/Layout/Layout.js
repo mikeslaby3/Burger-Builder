5d55d319b2ee9f69a14999a3c75650a9292571ed
Mike Slaby <mikeslaby3@gmail.com>
2019-08-27 14:16:58 -0700

    CSS sylesheet for layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>ToolBar, Sidedrawer, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
