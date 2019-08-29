84c284ec22032796184a199192247c37fd721885
Mike Slaby <mikeslaby3@gmail.com>
2019-08-28 18:38:30 -0700

    Added logic for building burger with ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,27 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+            });
+        })
+        .reduce((array, element) => {
+            return array.concat(element)
+        }, []);
+    
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
+    console.log(transformedIngredients);
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
