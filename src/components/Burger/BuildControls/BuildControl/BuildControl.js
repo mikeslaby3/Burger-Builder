ab4c4b67a69d6e7bbe874394a56e82fddf9ee685
Mike Slaby <mikeslaby3@gmail.com>
2019-08-28 19:41:42 -0700

    Passed remove prop from builder to controls to btn

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,13 @@ import classes from './BuildControl.module.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.ingredientLabel}</div>
-        <button className={classes.Remove}>Remove</button>
-        <button className={classes.Add} onClick={props.added}>Add</button>
+        <button 
+            className={classes.Remove} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Remove</button>
+        <button 
+            className={classes.Add} 
+            onClick={props.added}>Add</button>
     </div>
 )
 
