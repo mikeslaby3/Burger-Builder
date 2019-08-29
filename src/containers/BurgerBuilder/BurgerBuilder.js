1c3a008cdeec4330de52ee62fc518cb5601f8234
Mike Slaby <mikeslaby3@gmail.com>
2019-08-28 19:23:44 -0700

    Function that adds ingredients and price to burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,12 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    lettuce: 0.5,
+    cheese: 0.75,
+    meat: 2,
+    bacon: 1
+}
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -11,14 +17,34 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldIngredientCount = this.state.ingredients[type];
+        const updatedIngredientCount = oldIngredientCount + 1;
+        // state should be updated in an immutable way so we're creating
+        // a new object with the spread operator to distribute all the 
+        // properties in the old ingredients state into this new object
+        const updatedIngredients = {...this.state.ingredients}
+        updatedIngredients[type] = updatedIngredientCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        });
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                />
             </Aux>
         )
     }
