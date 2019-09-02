30ea6dc2376c184c0873c167808b223e2002b56a
Mike Slaby <mikeslaby3@gmail.com>
2019-09-02 15:30:00 -0700

    Toolbar containing logo and nav items

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
+
+const toolbar = (props) => {
+    return (
+        <header className={classes.Toolbar}>
+            <div>MENU</div>
+            <Logo />
+            <nav>
+                <NavItems />
+            </nav>
+        </header>
+    );
+}
+
+export default toolbar;
