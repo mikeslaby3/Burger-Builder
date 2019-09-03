f5a040f1697a51ca9b3f178c268d71b044eb1123
Mike Slaby <mikeslaby3@gmail.com>
2019-09-03 13:53:56 -0700

    Added open and close CSS logic

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,11 +7,16 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
-    //...
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
         <Aux>
-            <Backdrop show/>
-            <div className={classes.SideDrawer}>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
