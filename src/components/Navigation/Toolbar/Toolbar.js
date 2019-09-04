e573981b96b407eb7df784fad2d588d236b31125
Mike Slaby <mikeslaby3@gmail.com>
2019-09-03 23:26:59 -0700

    Connected side drawer toggle component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,12 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
-            <div>MENU</div>
+            <DrawerToggle clicked={props.drawerToggleClicked}/>
             <div className={classes.Logo}>
                 <Logo />
             </div>
